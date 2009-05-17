fd58a1c79df3c195587645d53cc045ef0129a382
davelnewton@gmail.com <davelnewton@gmail.com@dd822f30-ef12-11dd-ac65-f74f249cfaac>
2009-05-17 20:37:32 +0000

    Formatting.

--- trunk/ch02/convention/src/main/java/com/packt/s2wad/ch02/convention/actions/MoreresultsAction.java
+++ trunk/ch02/convention/src/main/java/com/packt/s2wad/ch02/convention/actions/MoreresultsAction.java
@@ -6,32 +6,43 @@ import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
 
 @Results({
-    @Result(name="nine", location="/WEB-INF/jsps/tristate/tristate-nine.jsp")
+        @Result(name = "nine", location = "/WEB-INF/jsps/tristate/tristate-nine.jsp")
 })
 public class MoreresultsAction extends ActionSupport {
     private long ctm;
-    
+
     public String execute() {
         ctm = System.currentTimeMillis();
         long tmp = getLastDigit();
-        if (tmp == 9) return "nine";
+        if (tmp == 9) {
+            return "nine";
+        }
         return ((ctm & 1L) == 1L) ? "odd" : "even";
     }
 
     @Action(value = "/tristate",
             results = {
-                @Result(name="three", location="/WEB-INF/jsps/tristate/tristate-three.jsp"),
-                @Result(name="six", location="/WEB-INF/jsps/tristate/tristate-six.jsp")
+                    @Result(name = "three", location = "/WEB-INF/jsps/tristate/tristate-three.jsp"),
+                    @Result(name = "six", location = "/WEB-INF/jsps/tristate/tristate-six.jsp")
             })
     public String tristate() {
         ctm = System.currentTimeMillis();
         long tmp = getLastDigit();
-        if (tmp <= 3) return "three";
-        if (tmp <= 6) return "six";
+        if (tmp <= 3) {
+            return "three";
+        }
+        if (tmp <= 6) {
+            return "six";
+        }
         return "nine";
     }
-    
-    private long getLastDigit() { return ctm - ((ctm / 10L) * 10L); }
 
-    public long getCtm() { return ctm; }
+    private long getLastDigit() {
+        return ctm - ((ctm / 10L) * 10L);
+    }
+
+    public long getCtm() {
+        return ctm;
+    }
+
 }
