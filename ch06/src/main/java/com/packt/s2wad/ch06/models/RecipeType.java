27c8f0a5b8d883964cbdca19adfdf7db85de9256
davelnewton <davelnewton@gmail.com>
2009-06-02 04:03:16 +0000

    Prep for recipe type user story.

--- ch06/src/main/java/com/packt/s2wad/ch06/models/RecipeType.java
+++ ch06/src/main/java/com/packt/s2wad/ch06/models/RecipeType.java
@@ -2,37 +2,33 @@ package com.packt.s2wad.ch06.models;
 
 public class RecipeType {
 
-    //~ Properties
-    
     private Integer id;
     private String name;
-    
-    //~ Implementation
 
     public RecipeType() {
         // Does nothing.
     }
-    
+
     public RecipeType(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
-    
+
     @Override
     public String toString() {
         return String.format("%s [id=%d, name=%s]", super.toString(), id, name);
     }
-  
+
     //~ Accessors
-    
+
     public Integer getId() {
         return id;
     }
-    
+
     public void setId(Integer id) {
         this.id = id;
     }
-    
+
     public String getName() {
         return name;
     }
