3a6a2648b9329c34673a24ee261c8defb01b8ea1
davelnewton@gmail.com <davelnewton@gmail.com@dd822f30-ef12-11dd-ac65-f74f249cfaac>
2009-06-02 17:06:46 +0000

    what's going on here???

---
+++ trunk/ch07/src/main/java/com/packt/s2wad/ch07/actions/examples/MessageGamesAction.java
@@ -0,0 +1,44 @@
+package com.packt.s2wad.ch07.actions.examples;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.packt.s2wad.ch07.actions.recipes.NewRecipeAction;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+
+@Results({
+        @Result(name = ActionSupport.INPUT,
+                location = "/WEB-INF/content/examples/message-games.jsp"),
+        @Result(name = ActionSupport.SUCCESS,
+                location = "/WEB-INF/content/examples/message-games.jsp")
+})
+public class MessageGamesAction extends ActionSupport {
+
+    private String name;
+
+    private static final Log LOG = LogFactory.getLog(MessageGamesAction.class);
+
+    @Override
+    @Action(value = "message-games")
+    public String input() throws Exception {
+        LOG.debug("Enter.");
+        return INPUT;
+    }
+
+    @Action(value = "message-games-process")
+    public String process() {
+        LOG.debug("Enter.");
+        return SUCCESS;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
